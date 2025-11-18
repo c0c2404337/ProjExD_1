02d29da8dc7bc6c0d2e53f9f652b00195980b7ce
24043 <c0c2404337@edu.teu.ac.jp>
2025-11-18 16:38:50 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -17,9 +17,10 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [300, 200])
+        
+        x = tmr
+        screen.blit(bg_img, [-x, 0])
+        screen.blit(kk_img, [300, 200]) #練習4
         pg.display.update()
         tmr += 1        
         clock.tick(10)
